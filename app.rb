f481be622892ae60ed06897bd751dd75cabe91fc
Oto Brglez <otobrglez@gmail.com>
2014-10-08 13:36:21 +0100

    Update

--- app.rb
+++ app.rb
@@ -59,23 +59,6 @@ __END__
     %link{rel: 'stylesheet', href: './app.css', type: 'text/css', media: 'all'}/
 
   %body
-    :javascript
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '771031166289168',
-          xfbml      : true,
-          version    : 'v2.1'
-        });
-      };
-
-      (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "//connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-
     %header#header
       .container
         %h1 Generator političnega blefa
@@ -97,6 +80,29 @@ __END__
         \|
         %a{href:"/all.html"} Seznam vseh.
 
+    :javascript
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '771031166289168',
+          xfbml      : true,
+          version    : 'v2.1'
+        });
+      };
 
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-2138405-31', 'auto');
+      ga('send', 'pageview');
 
 
