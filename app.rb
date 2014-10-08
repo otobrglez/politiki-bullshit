1e1552812a2835f446d877d6eec0ccff366e2098
Oto Brglez <otobrglez@gmail.com>
2014-10-08 16:03:17 +0100

    Update

--- app.rb
+++ app.rb
@@ -60,7 +60,10 @@ __END__
     %meta{name:"og:image", content:"./politiki_social.png"}
     %link{href:"http://fonts.googleapis.com/css?family=Roboto:700,400,300,100&subset=latin,latin-ext", rel:"stylesheet"}
     %link{rel: 'stylesheet', href: './app.css', type: 'text/css'}/
-
+    %link{rel:"icon", type:"image/png", href:"./favicon.png"}
+    %link{rel:"apple-touch-icon", href:"./apple-touch-icon.png"}
+    %link{rel:"apple-touch-icon", sizes:"72x72", href:"./apple-touch-icon-72x72.png"}
+    %link{rel:"apple-touch-icon", sizes:"114x114", href:"./apple-touch-icon-114x114.png"}
   %body
     %header#header
       .container
